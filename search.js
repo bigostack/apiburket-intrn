82fbc92fb31c45102feab2d0d0b12ddb27873343
unknown <garbajamiu2000@gmail.com>
2023-11-22 07:15:48 +0100

    remove the listening to my custom event

--- search.js
+++ search.js
@@ -27,36 +27,6 @@ async function getData() {
 
 getData();
 
-// LISTENING TO THE CUSTOM EVENT
-
-searchBar.addEventListener("build", (e) => {
-  let searchValue = searchBar.value;
-
-  //* This must be done to change the first letter of the input value to upperCase as the APIs name and Categories have their first letters in uppperCase
-
-  searchValue = searchValue.toLowerCase();
-
-  //* sets the criteria and push the items that matches the criteria into an array
-
-  let resultsList = [];
-  if (searchValue === "") {
-    console.log("No results match an empty search-string!");
-    return;
-  } else {
-    e.detail.forEach((elem) => {
-      if (elem.API.toLowerCase().includes(searchValue)) resultsList.push(elem);
-      else if (
-        elem.Category.toLowerCase().includes(searchValue) &&
-        searchValue.length >= 3
-      )
-        resultsList.push(elem);
-    });
-  }
-  console.log(resultsList);
-
-  //? To display the items, create a function that will go through the each items in the array(resultsList) and use the values insides it to diplay the content needed -> displayAPIs(arr).
-});
-
 //PAGINATION FUNCTIONALITY
 
 function pagination() {
